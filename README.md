9c694294de805766c40c285ba95b437d6dab578c
JahongirOdilov <odilovjahongir2001@gmail.com>
2023-11-25 12:27:21 +0500

    API images HARRY POTTER

---
+++ README.md
@@ -0,0 +1,14 @@
+# ---------- 20.11.23 ----------(74)
+
+# Functional Programming && Closure
+
+# JS Advanced
+
+    ✅ Functional Programming && Closure
+    ✅ Promises && Async/Await
+    ✅ Backend && AJAX && Fetch
+    ✅ JS Execution && Event Loop
+    ✅ NPM Package
+    ✅ Clean Code (https://github.com/ryanmcdermott/clean-code-javascript)
+
+# [CSBIN.IO](http://csbin.io)
---
+++ src/index.ts
@@ -0,0 +1,45 @@
+import "./css/index.css";
+import axios from "axios";
+
+/* DOM VARIABLES */
+const loader_1: HTMLDivElement = document.querySelector(".loader-1");
+const loader_2: NodeListOf<HTMLDivElement> = document.querySelectorAll(".loader-2");
+const container: HTMLElement = document.querySelector(".container");
+const boxElms: NodeListOf<HTMLDivElement> = document.querySelectorAll(".box");
+const btnMore: HTMLButtonElement = document.querySelector(".btn");
+
+/* LOGICAL VARIABLES */
+const API = "https://harry-potter-api-en.onrender.com/db";
+
+window.addEventListener("DOMContentLoaded", async () => {
+	container.style.display = "none";
+
+	setTimeout(async () => {
+		loader_1.classList.add("hidden");
+		loader_1.style.display = "none";
+		container.style.display = "grid";
+
+		await searchImg();
+	}, 3000);
+
+	async function searchImg() {
+		try {
+			const getInfoFromApi = await axios.get(API);
+			const characters = getInfoFromApi.data.characters;
+
+			boxElms.forEach((item, idx) => {
+				const person = characters[Math.floor(Math.random() * characters.length)];
+				const personImg = person.image;
+				const img = document.createElement("img");
+				img.src = personImg;
+				item.append(img);
+				loader_2[idx].style.display = "none";
+			});
+		} catch (error) {
+			loader_2.forEach((loaderItem) => {
+				loaderItem.style.display = "flex";
+			});
+			console.error(error);
+		}
+	}
+});
